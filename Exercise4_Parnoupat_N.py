02254d1d80a2aa1325ecd7f8439c0483429f49de
parnoupat <parnoupat.n@icloud.com>
2021-03-22 00:56:40 +0700

    Final Exercise
    ส่งการบ้าน

--- Exercise4_Parnoupat_N.py
+++ Exercise4_Parnoupat_N.py
@@ -1,1 +1,15 @@
-print("Hello")
+F_ENG = 80.0
+GEN_BUS = 90.8
+INT_COMS = 78.8
+COM_PRO = 98.7
+Total_score = F_ENG+GEN_BUS+INT_COMS+COM_PRO
+
+print("---------------- Your Score ----------------")
+print("Foundation English               : ", F_ENG)
+print("General Business                 : ", GEN_BUS)
+print("Introduction to Computer Systems : ", INT_COMS)
+print("Computer Programming             : ", COM_PRO)
+print("---------------- Total Score----------------")
+print("Total                            : ", Total_score)
+print("--------------- Average Score --------------")
+print("Average                          : ", Total_score/4)
