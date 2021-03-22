3e64c843e08d6935c5bd26e25d71af7679c5da30
parnoupat <parnoupat.n@icloud.com>
2021-03-22 22:39:12 +0700

    Exercise5_1

---
+++ Exercise5_1_Parnoupat_N.py
@@ -0,0 +1,11 @@
+num1 = int(input("ตัวเลขแรก : "))
+num2 = int(input("ตัวเลขที่สอง : "))
+plus = num1 + num2
+minus = num1 - num2
+time = num1 * num2
+devide = num1 / num2
+print("-----Your Result-----")
+print(num1," + ", num2," = ", plus)
+print(num1," - ", num2," = ", minus)
+print(num1," x ", num2," = ", time)
+print(num1," / ", num2," = ", devide)
