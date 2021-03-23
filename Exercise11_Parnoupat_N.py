64467318a98b8bcba9c297b15b0a6ae44feb971b
parnoupat <parnoupat.n@icloud.com>
2021-03-23 23:46:05 +0700

    Update Exercise11_Parnoupat_N.py

--- Exercise11_Parnoupat_N.py
+++ Exercise11_Parnoupat_N.py
@@ -4,7 +4,7 @@ height = int(input("ความสูงของพีรามิด : "))
 for i in range(height):
     space = height - i - 1
     i=2*i+1
-    print(" "*space + "*"*i + " "*space )
+    print(" "*space + "*"*i)
 
     # "   *   " แถวที่ 0 ว่าง 3  * 1
     # "  ***  " แถวที่ 1 ว่าง 2  * 3
